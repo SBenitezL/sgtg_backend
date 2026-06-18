f247be92b6405ca411bab9032f2a8997481f0c8f
Santiago <sbenitez1607@gmail.com>
2026-06-17 23:52:32 -0500

    Add: implementacion para la clase que se encarga de las reglas de negocio para el servicio de autenticacion

--- src/main/java/com/sgtg/backend/domain/use_cases/ManageAuthCUImplAdapter.java
+++ src/main/java/com/sgtg/backend/domain/use_cases/ManageAuthCUImplAdapter.java
@@ -1,14 +1,38 @@
 package com.sgtg.backend.domain.use_cases;
 
 import com.sgtg.backend.application.input.auth.ManageAuthCUIntPort;
+import com.sgtg.backend.application.output.ExceptionFormatterIntPort;
+import com.sgtg.backend.application.output.ManageAuthGatewayIntPort;
+import com.sgtg.backend.application.output.ManageAuthServiceGatewayIntPort;
+import com.sgtg.backend.domain.models.Usuario;
 
 public class ManageAuthCUImplAdapter implements ManageAuthCUIntPort {
 
+    private final ManageAuthServiceGatewayIntPort authServiceGateway;
+    private final ManageAuthGatewayIntPort persistenceAuthService;
+    private final ExceptionFormatterIntPort exceptionFormatter;
+
+    public ManageAuthCUImplAdapter(ManageAuthServiceGatewayIntPort authServiceGateway,
+            ManageAuthGatewayIntPort persistenceAuthService, ExceptionFormatterIntPort exceptionFormatter) {
+        this.authServiceGateway = authServiceGateway;
+        this.persistenceAuthService = persistenceAuthService;
+        this.exceptionFormatter = exceptionFormatter;
+    }
+
     @Override
     public String login(String email, String password) {
-
+        Usuario user = persistenceAuthService.findByEmail(email);
+        if (user == null) {
+            this.exceptionFormatter.returNoData("error.auth.user_not_found");
+        }
         // TODO Auto-generated method stub
         return null;
+    }
+
+    @Override
+    public boolean register(Usuario usuario) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'register'");
     }
 
 }
